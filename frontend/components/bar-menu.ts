aa4813eb88af28f818a024794f7ae67b57c3ed35
Dennis Regalado <35279373+dennisregalado@users.noreply.github.com>
2024-10-03 12:30:40 -0500

    wip

--- frontend/components/bar-menu.ts
+++ frontend/components/bar-menu.ts
@@ -5,12 +5,15 @@ export default () => ({
     menuHeight: 0,
     infoHeight: 0,
     init() {
-        this.resize()
+        this.resize() 
+        setTimeout(() => {
+            this.resize()
+        }, 100)
         window.addEventListener('resize', this.resize.bind(this), { signal: this.abortController.signal })
     },
     resize() {
         const hours = document.getElementById('hours')
-        const menu = document.getElementById('menus')
+        const menu = document.getElementById('menu')
 
         if (hours) {
             this.infoHeight = hours.offsetHeight + hours.offsetTop
--- frontend/components/newsletter.ts
+++ frontend/components/newsletter.ts
@@ -12,7 +12,11 @@ export default (listId: string) => ({
   success: false,
   error: null,
   loading: false,
-  async submit() {
+  async submit() { 
+    if (!this.email) {
+      return
+    }
+
     if (!listId) {
       this.error = "Missing List ID";
       return;

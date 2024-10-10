736be6e041cc8930269f35b9cc4041c45df21f11
martafontb <martafontb@gmail.com>
2024-10-10 14:08:54 +0200

    focal point qa

--- frontend/components/hours-info-fullscreen.ts
+++ frontend/components/hours-info-fullscreen.ts
@@ -7,29 +7,26 @@ import { swup } from "../entrypoints/swup";
 export default () => ({
   abortController: new AbortController(),
   abortControllerResize: new AbortController(),
-  raf: null,
   aspectRatio: window.innerWidth / window.innerHeight,
+
   transformX: 0,
   transformY: 0,
   transformScale: 1,
   transformWidth: window.innerWidth,
   transformHeight: window.innerWidth * (window.innerWidth / window.innerHeight),
   selectedIndex: 0,
-  offsetY: 0,
   visible: false,
   animating: false,
   pointer: {
     x: 0,
-    y: 0,
-    last: {
-      x: 0,
-      y: 0,
-    }
+    y: 0
   },
   fromPosition: {},
   fromLarge: false,
   mediaCount: 0,
   media: [],
+  timeOut: null,
+  offsetY: 0,
   contentReplace: () => { },
   init() {
     window.addEventListener('pointermove', this.move.bind(this), {
@@ -40,23 +37,15 @@ export default () => ({
       signal: this.abortControllerResize.signal
     })
 
-    window.addEventListener('fullscreen:close', this.closeFullscreen.bind(this), {
-      signal: this.abortController.signal
-    });
-
-    this.draw()
-
     this.contentReplace = () => {
-      setTimeout(() => {
+      this.timeOut = setTimeout(() => {
         const fullscreenElement = document.querySelector('[data-fullscreen]');
         if (!fullscreenElement) {
           console.error('Fullscreen element not found');
           return;
         }
-        
-        const fullscreenImages = document.querySelectorAll('[data-fullscreen-image]');
-  
-        this.media = Array.from(fullscreenImages).map((el) => {
+        this.mediaCount = document.querySelector('[data-fullscreen]')?.dataset.count
+        this.media = Array.from(document.querySelectorAll('[data-fullscreen-image]'))?.map((el) => {
           const width = parseFloat(el.dataset.width);
           const height = parseFloat(el.dataset.height);
           const aspectRatio = parseFloat(el.dataset.aspectRatio);
@@ -76,15 +65,14 @@ export default () => ({
   
         this.mediaCount = this.media.length;
         fullscreenElement.dataset.count = this.mediaCount.toString();
-  
         if (this.media.length > 0) {
-          this.selectedIndex = 0;
+          this.selectedIndex = Math.min(this.selectedIndex, this.media.length - 1);
           this.resize();
         } else {
           console.error('No media items found');
         }
       }, 100);
-    };
+    }
 
     this.contentReplace()
 
@@ -93,8 +81,10 @@ export default () => ({
     Alpine.effect(() => {
       if (typeof this.selectedIndex === 'number' && this.visible && !this.animating) {
         if (this.fromLarge) {
+
           this.$dispatch('fullscreen:index', this.selectedIndex)
         } else {
+
           this.$dispatch('fullscreen:index', this.selectedIndex - 1)
         }
       }
@@ -104,82 +94,67 @@ export default () => ({
     this.pointer.x = e.clientX
     this.pointer.y = e.clientY
   },
-  draw() {
-    const x = this.pointer.x
-    const y = this.pointer.y
-
-    this.pointer.last.x = lerp(this.pointer.last.x, x, 0.05)
-    this.pointer.last.y = lerp(this.pointer.last.y, y, 0.05)
-
-    this.raf = requestAnimationFrame(this.draw.bind(this))
-  },
   destroy() {
     this.abortController.abort()
     this.abortControllerResize.abort()
     swup.hooks.off('content:replace', this.contentReplace)
 
-    if (this.raf) {
-      cancelAnimationFrame(this.raf)
-    }
+    clearTimeout(this.timeOut)
   },
-  async openFullscreen(e, { large = false, index = 0, clickedElement }) {
-    if (this.animating || this.media.length === 0) {
-      return;
-    }
-
-    this.lockScroll();
-    this.draw();
+  async openFullscreen(e: MouseEvent, { large = true, index = 0, clickedElement }) {
+    if (this.animating) return
 
     const container = document.querySelector('[data-fullscreen-fixed]')
     const element = document.querySelector('[data-fullscreen]')
 
-    this.fromLarge = large;
-    this.selectedIndex = Math.min(index, this.media.length - 1);
+    this.lockScroll()
 
-    this.visible = true;
+    this.fromLarge = large
+    this.selectedIndex = index
 
-    this.pointer.x = e.clientX;
-    this.pointer.y = e.clientY;
-    this.pointer.last.x = e.clientX;
-    this.pointer.last.y = e.clientY;
+    this.visible = true
 
+    this.pointer.x = e.clientX
+    this.pointer.y = e.clientY
     
     this.fromPosition = this.getClickedImageDimensions(clickedElement);
 
-    if (!this.fromPosition) {
-      return;
-    }
+    this.aspectRatio = this.fromPosition.width / this.fromPosition.height
 
    // Calculate offsetY
    const targetWidth = window.innerWidth;
    const heightBasedOnAspectRatio = targetWidth / this.activeMedia.aspectRatio;
    const targetHeight = Math.max(heightBasedOnAspectRatio, window.innerHeight) - window.innerHeight;
-   this.offsetY = targetHeight / 2;
+   this.offsetY = targetHeight / 2
 
     element.style.position = 'fixed';
     element.style.top = -this.offsetY + 'px';
+
+
+    await this.onAnimateOpen(this.offsetY)
+
+
+    if (container) {
+      element.style.position = 'relative';
+      element.style.top = 'auto';
+      container.scrollTop = this.offsetY
+      this.offsetY = 0
+    }
 
     this.originalImage = {
       element: clickedElement,
       rect: this.fromPosition,
       scrollY: window.scrollY
     };
-
-    this.aspectRatio = this.fromPosition.width / this.fromPosition.height;
-
-    // if (window.innerWidth >= 1024) {
-    //   await this.onAnimateOpen();
-    // }
-
-    await this.onAnimateOpen();
+ 
     this.animating = false;
   },
   async closeFullscreen() {
-    if (this.animating) {
-      return;
-    }
+    if (this.animating) return
 
-    this.animating = true;
+    const container = document.querySelector('[data-fullscreen-fixed]')
+    const element = document.querySelector('[data-fullscreen]')
+
 
     if (this.originalImage && this.originalImage.element) {
       const currentRect = this.originalImage.element.getBoundingClientRect();
@@ -199,63 +174,61 @@ export default () => ({
       }
     }
 
+    this.offsetY = container?.scrollTop ?? 0
 
-    // if (window.innerWidth >= 1024) {
-    //   await this.onAnimateClose()
-    // } else {
-    //   await new Promise(resolve => setTimeout(resolve, 300));
-    // }
+    element.style.position = 'fixed';
+    element.style.top = -this.offsetY + 'px';
 
-    await this.onAnimateClose();
+    await this.onAnimateClose(this.offsetY)
 
+    this.offsetY = 0
     this.animating = false
     this.visible = false
 
-    if (this.raf) {
-      cancelAnimationFrame(this.raf)
-    }
-
     this.unlockScroll()
 
-    if (this.$refs.fullscreenFixed) {
-      this.$refs.fullscreenFixed.scrollTop = 0;
-    }
+    container?.scrollTo({ top: 0 })
   },
   nextImage() {
+    if (this.mediaCount === 0) {
+      console.warn('No media items available');
+      return;
+    }
+
     this.selectedIndex = (this.selectedIndex + 1) % this.mediaCount;
     
     this.$nextTick(() => {
       this.resize();
       this.$dispatch('fullscreen:index', this.selectedIndex);
-      if (this.$refs.fullscreenFixed) {
-        this.$refs.fullscreenFixed.scrollTop = 0;
-      }
+      document.querySelector('[data-fullscreen-fixed]')?.scrollTo({ top: 0 });
     });
   },
   advanceImage(index: number) {
     this.selectedIndex = index
-    document.querySelector('[data-fullscreen-fixed]')?.scrollTo({ top: 0 })
-
     this.resize()
+
+    const container = document.querySelector('[data-fullscreen-fixed]')
+    const targetWidth = window.innerWidth;
+    const heightBasedOnAspectRatio = targetWidth / this.activeMedia.aspectRatio;
+    const targetHeight = Math.max(heightBasedOnAspectRatio, window.innerHeight) - window.innerHeight;
+
+    container?.scrollTo({ top: targetHeight / 2 })
+
   },
   resize() {
     this.transformWidth = window.innerWidth;
-    if (this.activeMedia && this.activeMedia.aspectRatio) {
-      const heightBasedOnAspectRatio = this.transformWidth / this.activeMedia.aspectRatio;
-      this.transformHeight = Math.max(heightBasedOnAspectRatio, window.innerHeight);
-    } else {
-      this.transformHeight = window.innerHeight;
-    }
+    const heightBasedOnAspectRatio = this.transformWidth / this.activeMedia.aspectRatio;
+    this.transformHeight = Math.max(heightBasedOnAspectRatio, window.innerHeight);
+  },
+  getClickedImageDimensions(clickedElement) {
+    if (!clickedElement) return null;
+    return clickedElement.getBoundingClientRect();
   },
   getLargeImageDimensions() {
     const largeImage = document.querySelector('[data-large-image]')
     if (!largeImage) return
     const dimensions = largeImage.getBoundingClientRect()
     return dimensions
-  },
-  getClickedImageDimensions(clickedElement) {
-    if (!clickedElement) return null;
-    return clickedElement.getBoundingClientRect();
   },
   getSmallImageDimensions() {
     const smallImage = document.querySelector('[data-small-image]')
@@ -269,54 +242,40 @@ export default () => ({
     const dimensions = fullscreen.getBoundingClientRect()
     return dimensions
   },
-  async onAnimateOpen() {
-    if (!this.activeMedia) {
-      return;
-    }
-
+  async onAnimateOpen(offsetY: number = 0) {
     this.animating = true;
 
     const targetWidth = window.innerWidth;
-    const heightBasedOnAspectRatio = targetWidth / (this.activeMedia.aspectRatio || this.aspectRatio);
+    const heightBasedOnAspectRatio = targetWidth / this.activeMedia.aspectRatio;
     const targetHeight = Math.max(heightBasedOnAspectRatio, window.innerHeight);
-
-    console.log('Animation start:', { from: this.fromPosition, to: { width: targetWidth, height: targetHeight } });
 
     return await animate((progress) => {
       this.transformX = range(0, 1, this.fromPosition.left, 0, progress);
-      this.transformY = range(0, 1, this.fromPosition.top, 0, progress);
+      this.transformY = range(0, 1, this.fromPosition.top + offsetY, 0, progress);
       this.transformWidth = range(0, 1, this.fromPosition.width, targetWidth, progress);
       this.transformHeight = range(0, 1, this.fromPosition.height, targetHeight, progress);
     }, { duration: 1.2, easing: expoInOut }).finished;
   },
-  async onAnimateClose() {
+  async onAnimateClose(offsetY: number = 0) {
     this.animating = true
+    const fromTop = this.fromPosition.top - (document.querySelector('[data-fullscreen-fixed]')?.getBoundingClientRect().top ?? 0);
+    const startWidth = window.innerWidth;
+    const heightBasedOnAspectRatio = startWidth / this.activeMedia.aspectRatio;
+    const startHeight = Math.max(heightBasedOnAspectRatio, window.innerHeight);
   
-    const fullscreenFixed = document.querySelector('[data-fullscreen-fixed]');
-    const fullscreenFixedRect = fullscreenFixed?.getBoundingClientRect();
-    const fromTop = this.fromPosition.top - (fullscreenFixedRect?.top ?? 0);
-  
-    const startX = this.transformX;  // Use current transform X
-    const startY = this.transformY;  // Use current transform Y
-    const startWidth = this.transformWidth;
-    const startHeight = this.transformHeight;
-  
-
     return await animate((progress) => {
-      this.transformX = lerp(startX, this.fromPosition.left, progress);
-      this.transformY = lerp(startY, fromTop, progress);
-      this.transformWidth = lerp(startWidth, this.fromPosition.width, progress);
-      this.transformHeight = lerp(startHeight, this.fromPosition.height, progress);
-    }, { 
-      duration: 0.5, 
-      easing: easeOutQuad 
-    }).finished;
+    this.transformX = range(0, 1, 0, this.fromPosition.left, progress)
+    this.transformY = range(0, 1, 0, fromTop + offsetY, progress)
+    this.transformWidth = range(0, 1, startWidth, this.fromPosition.width, progress)
+    this.transformHeight = range(0, 1, startHeight, this.fromPosition.height, progress)
+
+  }, { duration: 1.4, easing: expoInOut }).finished
   },
   get clipPath() {
     if (!this.visible) return 'none';
 
     const left = this.transformX;
-    const top = this.transformY;
+    const top = this.transformY - this.offsetY;
     const right = window.innerWidth - (this.transformX + this.transformWidth);
     const bottom = window.innerHeight - (this.transformY + this.transformHeight);
 
